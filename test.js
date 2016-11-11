7778d6278a59b9685674e5ce480b6a59f1d4c976
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-11-11 14:44:54 -0600

    Add in new test data set and visualization

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
+var all = require('./testData.json');
+var user = all[1];
+var neighbors = all.splice(2);
+/*
 var user = [1, 2, 3, 4];
 var neighbors = [
   [2, 3, 4, 5],
   [4, 8, 9, 2],
   [5, 4, 2, 1]
 ]
+*/
 
 var ranker = require('./index.js');
-console.log(ranker.curate(user, neighbors));
+var position = ranker.curate(user, neighbors);
+console.log(position);
+console.log(neighbors[position]);
